2725373e040852b0a2fa782ab99fa39dfec5502e
Kirill-max-bit <soloncenkokirill64@gmail.com>
2025-01-12 08:38:36 +0000

    сделал задачу

---
+++ main8.py
@@ -0,0 +1,15 @@
+
+distance_km = 10
+
+
+distance_ft = 30
+
+
+distance_m = distance_ft * 0.305
+
+
+smaller_distance = min(distance_km, distance_m)
+
+
+print("Меньшее расстояние:", smaller_distance)
+
