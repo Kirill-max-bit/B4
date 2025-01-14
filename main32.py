4cd97325cdceb0c3d11ade8789c77fa101763dba
Kirill-max-bit <soloncenkokirill64@gmail.com>
2025-01-14 10:00:58 +0000

    сделал задачу

--- main32.py
+++ main32.py
@@ -1,29 +1,31 @@
-num = int(input("Введите четырехзначное число: "))
+def check_four_digit_number(n):
+    d1 = n // 1000
+    d2 = (n // 100) % 10
+    d3 = (n // 10) % 10
+    d4 = n % 10
+
+    sum_first_pair = d1 + d2
+    sum_last_pair = d3 + d4
+
+    total_sum = d1 + d2 + d3 + d4
+
+    product_digits = d1 * d2 * d3 * d4
+
+    a = 5
+
+    result_a = sum_first_pair == sum_last_pair
+    result_b = total_sum % 3 == 0
+    result_c = product_digits % 4 == 0
+    result_d = product_digits % a == 0
+
+    return {
+        "a": result_a,
+        "b": result_b,
+        "c": result_c,
+        "d": result_d
+    }
 
 
-a = num // 1000
-b = (num // 100) % 10
-c = (num // 10) % 10
-d = num % 10
-
-
-if a + b == c + d:
-    print("Сумма двух первых цифр равна сумме двух последних цифр")
-else:
-    print("Сумма двух первых цифр не равна сумме двух последних цифр")
-
-if (a + b + c + d) % 3 == 0:
-    print("Сумма цифр кратна трем")
-else:
-    print("Сумма цифр не кратна трем")
-
-if (a * b * c * d) % 4 == 0:
-    print("Произведение цифр кратно четырем")
-else:
-    print("Произведение цифр не кратно четырем")
-
-a = int(input("Введите число а: "))
-if (a * b * c * d) % a == 0:
-    print("Произведение цифр кратно числу а")
-else:
-    print("Произведение цифр не кратно числу а")
+number = 1234
+results = check_four_digit_number(number)
+print(results)
