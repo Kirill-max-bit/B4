366b7ce7640ba9f306da6b5db9f8d86401784bdf
Kirill-max-bit <soloncenkokirill64@gmail.com>
2025-01-15 15:15:18 +0000

    сделал задачу

--- main47.py
+++ main47.py
@@ -1,5 +1,5 @@
 a = int(input("Введите число a: "))
-b = int(input("Введите число b: "))
+b = int(input("Введите число b:"))
 c = int(input("Введите число c: "))
 
 if a == b > c:
---
+++ main52.py
@@ -0,0 +1,9 @@
+a = int(input("Введите число a: "))
+b = int(input("Введите число b: "))
+d = int(input("Введите число c: "))
+
+if d + 2 >= a and d + 2 <= b:
+    print ('YES')
+else:
+    print('NO')
+    
